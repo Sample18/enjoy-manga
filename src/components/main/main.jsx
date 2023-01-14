3680d7a350f727dbab33295a5db1c1427cde04d2
Sample18 <Sample417@yandex.ru>
2023-01-14 15:22:36 +0300

    new design, using bootstrap, add linter

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import API from "../api";
 import _ from "lodash";
-import styles from "./main.module.css";
 import ChapterCard from "../chapterCard/chapterCard";
+// import styles from "./main.module.css";
 
 const Main = () => {
-  const { new_chapters } = styles;
-  const [chapters, setChapters] = useState();
+    const [chapters, setChapters] = useState();
 
-  useEffect(() => {
-    API.product
-      .getChapters()
-      .then((data) => setChapters(_.orderBy(data, ["date"], ["desc"])));
-  }, []);
-  return (
-    <div className={new_chapters}>
-      {chapters && chapters.map((chapter) => <ChapterCard {...chapter} />)}
-    </div>
-  );
+    useEffect(() => {
+        API.product
+            .getChapters()
+            .then((data) => setChapters(_.orderBy(data, ["date"], ["desc"])));
+    }, []);
+    return (
+        <div className="container d-flex flex-column px-1 py-4 bg-dark">
+            {chapters &&
+                chapters.map((chapter) => (
+                    <ChapterCard key={chapter.id + chapter.name} {...chapter} />
+                ))}
+        </div>
+    );
 };
 
 export default Main;
